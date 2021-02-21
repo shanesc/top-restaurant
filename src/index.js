eec744194b12433efe6a144efda4264485fda5aa
Shane Mitchell <shaneSC@gmail.com>
2021-02-21 15:14:47 +0000

    add functionality for nav links and module loading

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
+import header from './header';
 import home from './pages/home';
+import menu from './pages/menu';
+import contact from './pages/contact';
 
 const content = document.querySelector('#content');
 
+content.appendChild(header('Home', 'Menu', 'Contact'));
 content.appendChild(home);
+
+const homeLink = document.querySelector('#home');
+homeLink.addEventListener('click', () => {
+  removeLastChild();
+  content.appendChild(home);
+});
+
+const menuLink = document.querySelector('#menu');
+menuLink.addEventListener('click', () => {
+  removeLastChild();
+  content.appendChild(menu);
+});
+
+const contactLink = document.querySelector('#contact');
+contactLink.addEventListener('click', () => {
+  removeLastChild();
+  content.appendChild(contact);
+});
+
+function removeLastChild() {
+  if (content.childElementCount === 2) {
+    content.removeChild(content.lastChild);
+  }
+}
