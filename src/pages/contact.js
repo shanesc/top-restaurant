65eec5d76b561f6c9ea6b59202987a5b32577848
Shane Mitchell <shaneSC@gmail.com>
2021-02-22 16:46:08 +0000

    add contact module

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,26 @@
 function contact() {
   const el = document.createElement('div');
-  el.innerHTML = 'Contact';
+
+  const h1 = document.createElement('h1');
+  h1.innerHTML = 'Contact';
+
+  const list = document.createElement('ul');
+  list.classList.add('contact-list');
+
+  const address = document.createElement('li');
+  address.innerHTML = '123 Cactus Lane, Bristol, UK';
+  const email = document.createElement('li');
+  email.innerHTML = 'spicyhot@tacosforall.com';
+  const phone = document.createElement('li');
+  phone.innerHTML = '07123 45678';
+
+  list.appendChild(address);
+  list.appendChild(email);
+  list.appendChild(phone);
+
+  el.appendChild(h1);
+  el.appendChild(list);
+
   return el;
 }
 
